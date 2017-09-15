660ed33a5f19cb15c7690565a7e40ebcfbbd3add
Nick Hall <nickhall122@gmail.com>
2017-09-15 18:21:32 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,26 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
+#binding.pry
     # code goes here
+    #binding.pry
+    newarray = []
+      self.collect do |key, value|
+        #binding.pry
+        if arguments.include?(value)
+          newarray.push(key)
+          #binding.pry
+        #.collect do |value|
+        #binding.pry
+          #if x == arguments
+            #newarray.push(x)
+        #  y.each do |x|
+            #binding.pry
+        #  end
+        #binding.pry
+
+        end
+      end
+      #binding.pry
+      newarray
   end
 end
